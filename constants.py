01f05d7f0d089efa1e7147a0cf444736da64c62e
Ahmed KHABER <ahmed.gsm1983@gmail.com>
2025-07-26 22:30:45 +0100

    Change notifications values messages and delay

--- constants.py
+++ constants.py
@@ -1,13 +1,16 @@
 
 NOTIFICATION_HIDE_FOR_MODERATION = " سيتم التحقق من المنشور يرجى الانتظار، شكرا"
 NOTIFICATION_INSERT_PRODUCT_DEF = (
-    "\n المنشور مرفوض"
-    " يرجى كتابة اسم ووصف المنتج"
+    " \n\n"
+    " يرجى كتابة إسم ووصف المنتج\n"
+    " \n\n\n"
 )
 NOTIFICATION_NO_QUESTIONS = (
+    "\n\n\n"
     "لطرح الإستفسارات أو الأسئلة، أو طلب منتجات، يرجى زيارة المجموعة التالية: "
+    "\n\n\n"
     "https://t.me/+B8OaGveY395hODQ0\n"
 )
 
 
-DELETE_NOTIFICATION_DELAY = 10
+DELETE_NOTIFICATION_DELAY = 12
